39382d06a9e90f50a6dcb7dc316fd1df35d7a7a9
Porpora <porpora.f@pg.com>
2024-10-19 12:45:53 +0200

    connect with Csv

--- MainPyFile.py
+++ MainPyFile.py
@@ -14,3 +14,11 @@ print (array_3,'shape',np.shape(array_2))
 print('Array 3_',array_3)
 array_4=array_2*array_3
 print('Matrice:',array_4)
+import csv
+csv_filename='eggs.csv'
+with open(csv_filename, 'r') as fp:
+    reader = csv.reader(fp)
+    data = list(reader)
+#array_data = np.array(data)
+
+print(data)
