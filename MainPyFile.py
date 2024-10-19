5d5f1758fdd50841439ea525a176ebfda526c725
Porpora <porpora.f@pg.com>
2024-10-19 12:05:38 +0200

    primo test

---
+++ MainPyFile.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+array = np.array([1,2,3])
+print (array)
+
+array_2 = np.append(array,[77])
+print (array_2,'shape',np.shape(array_2))
+ 
+array_3 = np.random.randint(4)
+print (array_3,'shape',np.shape(array_3))
+array_3=np.transpose(array_3)
+print (array_3,'shape',np.shape(array_2))
+print('Array 3_',array_3)
+array_4=array_2*array_3
+print('Matrice:',array_4)
