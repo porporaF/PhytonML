b64a558390b2b2f16840c0b3af539f64e4109496
Porpora <porpora.f@pg.com>
2024-10-19 14:22:23 +0200

    function

--- MainPyFile.py
+++ MainPyFile.py
@@ -1,23 +1,12 @@
 import pandas as pd
-import numpy as np
 
-array = np.array([[1,2,3],[77,78,79]])
-print (array)
+def converti (file_path):
+   
+   df = pd.read_csv(file_path) 
+   data = pd.DataFrame(df)
+   return data
 
-array_2 = np.append(array,[77,90,99])
-print (array_2,'shape',np.shape(array_2))
- 
-array_3 = np.random.randint(4)
-print (array_3,'shape',np.shape(array_3))
-array_3=np.transpose(array_3)
-print (array_3,'shape',np.shape(array_2))
-print('Array 3_',array_3)
-array_4=array_2*array_3
-print('Matrice:',array_4)
-import csv
-csv_filename='eggs.csv'
-df = pd.read_csv(csv_filename)
-#array8data
+path='eggs.csv'
+dataC = converti(path)
+print (dataC)
 
-print (df)
-print (df["data"].mean())
