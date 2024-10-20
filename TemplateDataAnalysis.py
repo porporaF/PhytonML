755574fd3679aa670f9696623fe5f22fc6ab1272
Porpora <porpora.f@pg.com>
2024-10-20 21:09:20 +0200

    polinomiale

--- TemplateDataAnalysis.py
+++ TemplateDataAnalysis.py
@@ -8,10 +8,9 @@ from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import mean_squared_error, r2_score
 import warnings
+from numpy.polynomial.polynomial import polyval
 warnings.filterwarnings("ignore", category=UserWarning) 
-
-#%matplotlib inline
-
+     
 # Importing Data set
 #from pyodide.http import pyfetch
 
@@ -35,22 +34,82 @@ print(df)
 
 #Crea linear Regression 
 lm = LinearRegression()
-X=df["CPU_frequency"]
-Y=df["Price"]
+X=df[['CPU_frequency']]
+Y=df['Price']
+
 
 plt.scatter(X,Y)
 plt.xlabel("CPU_Frequency")
 plt.ylabel("Price")
 plt.show()
 
-Yhat=lm.fit(X,Y)
+lm.fit(X,Y)
+Yhat=lm.predict(X)
+
 lm.coef_
 lm.intercept_
 
-plt.scatter(X,Yhat)
-plt.xlabel("CPU_Frequency")
-plt.ylabel("Price")
+ax1 = sns.distplot(df['Price'], hist=False, color="r", label="Actual Value")
+
+# Create a distribution plot for predicted values
+sns.distplot(Yhat, hist=False, color="b", label="Fitted Values" , ax=ax1)
+
+plt.title('Actual vs Fitted Values for Price')
+plt.xlabel('Price')
+plt.ylabel('Proportion of laptops')
+plt.legend(['Actual Value', 'Predicted Value'])
+plt.show()
+# Regressione a 2 parametri
+lm2 = LinearRegression()
+Z=df[['CPU_frequency','RAM_GB']]
+Y=df['Price']
+
+lm2.fit(Z,Y)
+Yhat=lm2.predict(Z)
+print(lm2.coef_)
+print(lm2.intercept_)
+
+ax1 = sns.distplot(df['Price'], hist=False, color="r", label="Actual Value")
+
+# Create a distribution plot for predicted values
+sns.distplot(Yhat, hist=False, color="b", label="Fitted Values" , ax=ax1)
+
+plt.title('Actual vs Fitted Values for Price')
+plt.xlabel('Price')
+plt.ylabel('Proportion of laptops')
+plt.legend(['Actual Value', 'Predicted Value'])
 plt.show()
 
+#Polinomiale
+
+X = X.to_numpy().flatten()
+f1 = np.polyfit(X, Y, 1)
+p1 = np.poly1d(f1)
+f3 = np.polyfit(X, Y, 3)
+p3 = np.poly1d(f3)
+f5 = np.polyfit(X, Y, 5)
+p5 = np.poly1d(f5)
 
 
+def PlotPolly(model, independent_variable, dependent_variabble, Name):
+    x_new = np.linspace(independent_variable.min(),independent_variable.max(),100)
+    y_new = model(x_new)
+    plt.plot(independent_variable, dependent_variabble, '.', x_new, y_new, '-')
+    plt.title(f'Polynomial Fit for Price ~ {Name}')
+    ax = plt.gca()
+    ax.set_facecolor((0.898, 0.898, 0.898))
+    fig = plt.gcf()
+    plt.xlabel(Name)
+    plt.ylabel('Price of laptops')
+   
+PlotPolly(p1, X, Y, 'CPU_frequency')
+plt.show()
+PlotPolly(p3, X, Y, 'CPU_frequency')
+plt.show()
+PlotPolly(p5, X, Y, 'CPU_frequency')
+plt.show()
+
+print (p5.coef)
+
+a=polyval(1,p5.coef)
+print('a=',a)
